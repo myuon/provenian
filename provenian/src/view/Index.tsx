c658663591d1a8de68036f23c6992d638cae0c96
myuon <ioi.joi.koi.loi@gmail.com>
2019-08-11 05:07:07 +0900

    design(front): lovely picture

--- provenian/src/view/Index.tsx
+++ provenian/src/view/Index.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Header } from "semantic-ui-react";
+import { Header, Image } from "semantic-ui-react";
+import top_neko from "../top_neko.jpeg";
 
 const Index: React.FC = () => {
   return (
     <>
+      <Header as="h2">ようこそ</Header>
+      <Image src={`${process.env.PUBLIC_URL}/top_neko.jpg`} />
+
       <Header as="h2">問題一覧</Header>
       <ul>
         <li>
           <Link to={"/problems/rev-append"}>Reversely Appended</Link>
+        </li>
+        <li>
+          <Link to={"/problems/sum-1-n"}>Sums up to N</Link>
         </li>
       </ul>
     </>
