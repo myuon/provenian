5a13a6fb5159165a555899d3fce4729bca0b0156
myuon <ioi.joi.koi.loi@gmail.com>
2019-08-11 19:35:08 +0900

    design(front): centering

--- provenian/src/view/Index.tsx
+++ provenian/src/view/Index.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Header, Image } from "semantic-ui-react";
+import { Header, Image, Grid } from "semantic-ui-react";
 
 const Index: React.FC = () => {
   return (
-    <>
-      <Header as="h2">ようこそ</Header>
-      <Image src={`${process.env.PUBLIC_URL}/top_neko.jpg`} />
+    <Grid centered>
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <Header as="h2">ようこそ</Header>
+          <Image src={`${process.env.PUBLIC_URL}/top_neko.jpg`} />
 
-      <Header as="h2">問題一覧</Header>
-      <ul>
-        <li>
-          <Link to={"/problems/rev-append"}>Reversely Appended</Link>
-        </li>
-        <li>
-          <Link to={"/problems/sum-1-n"}>Sums up to N</Link>
-        </li>
-      </ul>
-    </>
+          <Header as="h2">問題一覧</Header>
+          <ul>
+            <li>
+              <Link to={"/problems/rev-append"}>Reversely Appended</Link>
+            </li>
+            <li>
+              <Link to={"/problems/sum-1-n"}>Sums up to N</Link>
+            </li>
+          </ul>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   );
 };
 
